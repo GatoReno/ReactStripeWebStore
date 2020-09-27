5d1144380009fdc33ba79d960fe1469b1e78cd40
Gatoreno <erp.reno@hotmail.com>
2020-09-27 13:51:27 -0400

    stripe installed and input card added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
 import React from 'react';
 import logo from './logo.svg';
+import  {loadStripe} from '@stripe/stripe-js';
+import {Elements,
+        CardElement} from '@stripe/react-stripe-js';
+ 
 import './App.css';
 
+const stripePromise = loadStripe('pk_test_Qt5RGMIGZjmsSsuPq0YbcnLJ');
+const CheckoutForm = () => {
+  const handleSubmit = (e)=> {
+    e.preventDefault();
+    console.log('submited');
+
+  };
+  return <form onSubmit={handleSubmit}>
+    <CardElement/> 
+    <button>
+      Buy
+    </button>
+  </form>
+};
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Elements stripe={stripePromise}>
+      <CheckoutForm/>
+    </Elements>
   );
-}
+};
 
 export default App;
